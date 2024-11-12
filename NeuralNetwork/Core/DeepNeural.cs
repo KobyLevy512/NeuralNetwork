ebfa03ae4b80554d3cb8513866509a71e3e432a6
kobylevy <kobylevy@KOBYLEVYBA2D>
2024-11-12 09:08:03 +0200

    Simplify the network with Neuron Layer class. Create the Deep Neural class.

---
+++ NeuralNetwork/Core/DeepNeural.cs
@@ -0,0 +1,55 @@
+﻿
+using static NeuralNetwork.Core.ActivationFunction;
+
+namespace NeuralNetwork.Core
+{
+    public class DeepNeural
+    {
+        // Random generator for weight initialization
+        private Random random;
+
+        //Layers
+        public List<NeuronLayer> HiddenLayers;
+        private double[] output;
+
+        //Learning Info.
+        public Activation Activation, ActivationDerivative;
+        public int Seed;
+
+        public DeepNeural(DeepNeural cpy, int newSeed = 0)
+        {
+            if (newSeed != 0) Seed = newSeed;
+            else Seed = cpy.Seed;
+            random = new Random(Seed);
+
+            Activation = cpy.Activation;
+            ActivationDerivative = cpy.ActivationDerivative;
+
+            output = new double[cpy.output.Length];
+
+            HiddenLayers = new List<NeuronLayer>();
+            for(int i = 0; i < cpy.HiddenLayers.Count; i++)
+            {
+                HiddenLayers.Add(new NeuronLayer(cpy.HiddenLayers[i]));
+            }
+        }
+        public DeepNeural(int outputSize):this(outputSize, 0) { }
+        public DeepNeural(int outputSize, int seed):this(outputSize, seed, Sigmoid, SigmoidDerivative) { }
+        public DeepNeural(int outputsSize, int seed, Activation act, Activation der)
+        {
+            HiddenLayers = new List<NeuronLayer>();
+            output = new double[outputsSize];
+            Activation = act;
+            ActivationDerivative = der;
+
+            if(seed != 0)
+            {
+                random = new Random(seed);
+            }
+            else
+            {
+                random = new Random();
+            }
+        }
+    }
+}
---
+++ NeuralNetwork/Core/NeuronLayer.cs
@@ -0,0 +1,60 @@
+﻿
+using static NeuralNetwork.Core.ActivationFunction;
+
+namespace NeuralNetwork.Core
+{
+    public class NeuronLayer
+    {
+        private double[,] weightsInput;
+        private double[,] weightsOutput;
+        private double[] output;
+
+        public NeuronLayer(NeuronLayer cpy)
+        {
+            weightsInput = new double[cpy.weightsInput.GetLength(0), cpy.weightsInput.GetLength(1)];
+            weightsOutput = new double[cpy.weightsOutput.GetLength(0), cpy.weightsOutput.GetLength(1)];
+            output = new double[cpy.output.Length];
+        }
+        public NeuronLayer(int inputs, int nodes, int outputs)
+        {
+            weightsInput = new double[inputs, nodes];
+            weightsOutput = new double[nodes, outputs];
+            output = new double[nodes];   
+        }
+
+        public void PassInput(double[] input, Activation activation)
+        {
+            if(input.Length != weightsInput.GetLength(0))
+            {
+                throw new ArgumentException("inputs array size is not match");
+            }
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                double sum = 0.0;
+                for (int j = 0; j < input.Length; j++)
+                {
+                    sum += input[j] * weightsInput[j, i];
+                }
+                output[i] = activation.Invoke(sum);
+            }
+        }
+
+        public void PassOutput(double[] output, Activation activation)
+        {
+            if (output.Length != weightsOutput.GetLength(1))
+            {
+                throw new ArgumentException("inputs array size is not match");
+            }
+            for (int i = 0; i < output.Length; i++)
+            {
+                double sum = 0.0;
+                for (int j = 0; j < this.output.Length; j++)
+                {
+                    sum += this.output[j] * weightsOutput[j, i];
+                }
+                output[i] = activation.Invoke(sum);
+            }
+        }
+    }
+}
