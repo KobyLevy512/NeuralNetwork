fdee02254f2f56aa4159916c85ea294231afa6c2
kobylevy <kobylevy@KOBYLEVYBA2D>
2024-11-12 12:29:26 +0200

    Implement the Deep Neural class.

--- NeuralNetwork/Core/DeepNeural.cs
+++ NeuralNetwork/Core/DeepNeural.cs
@@ -18,15 +18,17 @@ namespace NeuralNetwork.Core
 
         public DeepNeural(DeepNeural cpy, int newSeed = 0)
         {
+            //Set the new seed if supply
             if (newSeed != 0) Seed = newSeed;
             else Seed = cpy.Seed;
             random = new Random(Seed);
 
+            //Set Activations.
             Activation = cpy.Activation;
             ActivationDerivative = cpy.ActivationDerivative;
 
+            //Set the layers.
             output = new double[cpy.output.Length];
-
             HiddenLayers = new List<NeuronLayer>();
             for(int i = 0; i < cpy.HiddenLayers.Count; i++)
             {
@@ -37,18 +39,89 @@ namespace NeuralNetwork.Core
         public DeepNeural(int outputSize, int seed):this(outputSize, seed, Sigmoid, SigmoidDerivative) { }
         public DeepNeural(int outputsSize, int seed, Activation act, Activation der)
         {
+            //Set the layers.
             HiddenLayers = new List<NeuronLayer>();
             output = new double[outputsSize];
+
+            //Set activations.
             Activation = act;
             ActivationDerivative = der;
 
-            if(seed != 0)
+            //Apply the seed.
+            Seed = seed;
+            if(Seed != 0)
             {
-                random = new Random(seed);
+                random = new Random(Seed);
             }
             else
             {
                 random = new Random();
+            }
+        }
+
+        /// <summary>
+        /// Reset all the weights.
+        /// </summary>
+        /// <param name="pink"></param>
+        public void ResetWeights(bool pink = false)
+        {
+            for(int i = 0; i < HiddenLayers.Count; i++) 
+            {
+                HiddenLayers[i].Reset(random, pink);
+            }
+        }
+
+        public double[] Forward(double[] inputs)
+        {   
+            //Pass all the hidden layers.
+            for(int i = 0; i < HiddenLayers.Count; i++)
+            {
+                double[] output = new double[HiddenLayers[i].ExpectedOutput];
+                HiddenLayers[i].Pass(inputs, output, Activation);
+                inputs = output;
+            }
+
+            //Apply the result and return.
+            output = inputs;
+            return output;
+        }
+
+        public void Train(double[] inputs, double[] targets, double learningRate)
+        {
+            //Calculate output deltas.
+            double[] outputDeltas = new double[output.Length];
+            for (int i = 0; i < outputDeltas.Length; i++)
+            {
+                double error = targets[i] - output[i];
+                outputDeltas[i] = error * ActivationDerivative(output[i]);
+            }
+
+            //Pass all the hidden layers and update them.
+            for (int i = 0; i < HiddenLayers.Count; i++)
+            {
+                double[] output = new double[HiddenLayers[i].ExpectedOutput];
+                HiddenLayers[i].Pass(inputs, output, Activation);
+                HiddenLayers[i].UpdateWeights(inputs, outputDeltas, ActivationDerivative, learningRate);
+                inputs = output;
+            }
+        }
+        public void Backpropagate(double[] inputs, double[] targets, double learningRate = 0.1)
+        {
+            //Calculate output deltas.
+            double[] outputDeltas = new double[output.Length];
+            for (int i = 0; i < outputDeltas.Length; i++)
+            {
+                double error = targets[i] - output[i];
+                outputDeltas[i] = error * ActivationDerivative(output[i]);
+            }
+
+            //Update the Weights for all hidden layers.
+            for (int i = 0; i < HiddenLayers.Count; i++)
+            {
+                double[] output = new double[HiddenLayers[i].ExpectedOutput];
+                HiddenLayers[i].UpdateWeights(inputs, outputDeltas, ActivationDerivative, learningRate);
+                HiddenLayers[i].Pass(inputs, output, Activation);
+                inputs = output;
             }
         }
     }
--- NeuralNetwork/Core/NeuronLayer.cs
+++ NeuralNetwork/Core/NeuronLayer.cs
@@ -5,10 +5,20 @@ namespace NeuralNetwork.Core
 {
     public class NeuronLayer
     {
+        private const int pinkSize = 16;
         private double[,] weightsInput;
         private double[,] weightsOutput;
         private double[] output;
 
+        public int ExpectedOutput
+        {
+            get => weightsOutput.GetLength(1);
+        }
+
+        public int NodesAmount
+        {
+            get => output.Length;
+        }
         public NeuronLayer(NeuronLayer cpy)
         {
             weightsInput = new double[cpy.weightsInput.GetLength(0), cpy.weightsInput.GetLength(1)];
@@ -22,6 +32,47 @@ namespace NeuralNetwork.Core
             output = new double[nodes];   
         }
 
+        /// <summary>
+        /// Reset the Weights values.
+        /// </summary>
+        /// <param name="rnd"></param>
+        /// <param name="pink"></param>
+        public void Reset(Random rnd, bool pink)
+        {
+            if(pink)
+            {
+                for (int y = 0; y < weightsInput.GetLength(0); y++)
+                {
+                    for (int x = 0; x < weightsInput.GetLength(1); x++)
+                    {
+                        double pinkSum = 0;
+                        for(int i = 0; i < pinkSize; i++)
+                        {
+                            pinkSum += rnd.NextDouble();
+                        }
+                        pinkSum /= pinkSize;
+                        weightsInput[y, x] = pinkSum * 2 - 1;
+                    }
+                }
+            }
+            else
+            {
+                for (int y = 0; y < weightsInput.GetLength(0); y++)
+                {
+                    for (int x = 0; x < weightsInput.GetLength(1); x++)
+                    {
+                        weightsInput[y, x] = rnd.NextDouble() * 2 - 1;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Calculate this layer by inputs and pass values to the hidden layer.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="activation"></param>
+        /// <exception cref="ArgumentException"></exception>
         public void PassInput(double[] input, Activation activation)
         {
             if(input.Length != weightsInput.GetLength(0))
@@ -40,6 +91,12 @@ namespace NeuralNetwork.Core
             }
         }
 
+        /// <summary>
+        /// Calculate this layer output.
+        /// </summary>
+        /// <param name="output"></param>
+        /// <param name="activation"></param>
+        /// <exception cref="ArgumentException"></exception>
         public void PassOutput(double[] output, Activation activation)
         {
             if (output.Length != weightsOutput.GetLength(1))
@@ -54,6 +111,55 @@ namespace NeuralNetwork.Core
                     sum += this.output[j] * weightsOutput[j, i];
                 }
                 output[i] = activation.Invoke(sum);
+            }
+        }
+
+        /// <summary>
+        /// Pass inputs to the weights and get the result to the output array.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="output"></param>
+        /// <param name="act"></param>
+        public void Pass(double[] input, double[] output, Activation act)
+        {
+            PassInput(input, act);
+            PassOutput(output, act);
+        }
+
+        /// <summary>
+        /// Update the weights in Backpropagate.
+        /// </summary>
+        /// <param name="errorDelats"></param>
+        public void UpdateWeights(double[] inputs, double[] outputDeltas, Activation actDer, double learningRate)
+        {
+            //Calculate deltas.
+            double[] hiddenDeltas = new double[output.Length];
+            for (int i = 0; i < output.Length; i++)
+            {
+                double error = 0.0;
+                for (int j = 0; j < outputDeltas.Length; j++)
+                {
+                    error += outputDeltas[j] * weightsOutput[i, j];
+                }
+                hiddenDeltas[i] = error * actDer(output[i]);
+            }
+
+            //Update output weights.
+            for (int i = 0; i < weightsOutput.GetLength(0); i++)
+            {
+                for (int j = 0; j < weightsOutput.GetLength(1); j++)
+                {
+                    weightsOutput[i, j] += learningRate * outputDeltas[j] * output[i];
+                }
+            }
+
+            //Update input weights.
+            for (int i = 0; i < weightsInput.GetLength(0); i++)
+            {
+                for (int j = 0; j < weightsInput.GetLength(1); j++)
+                {
+                    weightsInput[i, j] += learningRate * hiddenDeltas[j] * inputs[i];
+                }
             }
         }
     }
