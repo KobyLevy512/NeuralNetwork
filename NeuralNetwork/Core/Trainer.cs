4892b8b165151cdfe78d609b76ea878d87f2e833
kobyl <kobyl@KOBY>
2024-11-14 12:59:53 +0200

    Add Train Result class that can return some data about the training process.

--- NeuralNetwork/Core/Trainer.cs
+++ NeuralNetwork/Core/Trainer.cs
@@ -5,14 +5,27 @@ namespace NeuralNetwork.Core
 {
     public class Trainer
     {
+        public class TrainResult
+        {
+            public int TotalSeeds;
+            public int Threads;
+            public Dictionary<int, int> SeedsByThread = new Dictionary<int, int>();
+            public double BestValue;
+            public double TotalTime;
+            public int[] SeedsStart;
+        }
+
         int threads, epochs;
         double minutesToTrain;
+        TrainResult? result;
+
         public Trainer(double minutesToTrain, int thread, int epochs)
         {
             this.minutesToTrain = minutesToTrain;
             this.threads = thread;
             this.epochs = epochs;
         }
+
         private int[] BuildSeedSteps(int size)
         {
             int[] seedSteps = new int[size];
@@ -25,22 +38,35 @@ namespace NeuralNetwork.Core
             return seedSteps;
         }
 
+        public TrainResult? GetLastTrainResult()
+        {
+            if (result == null) return result;
+            result.TotalSeeds = 0;
+            foreach(var key in result.SeedsByThread.Keys)
+            {
+                result.TotalSeeds += result.SeedsByThread[key];
+            }
+            return result;
+        }
         public BigDN? Train(BigDN network, DataStream stream)
         {
+            result = new TrainResult();
+            result.Threads = threads;
             BigDN? best = null;
             double bestValue = double.MaxValue;
             Task[] tasks = new Task[threads];
             int[] seedSteps = BuildSeedSteps(threads);
+            result.SeedsStart = seedSteps;
             Stopwatch sw = Stopwatch.StartNew();
             //Create all the threads, each thread respect other seed ranges.
             for (int i = 0; i < tasks.Length; i++)
             {
                 //Get the starting seed.
                 int seed = seedSteps[i];
+                result.SeedsByThread.Add(i, 0);
                 tasks[i] = Task.Run(() =>
                 {
                     int it = 0;//Iterations amount.
-
                     //While we still have time and the seed value is not collapse with other thread.
                     while (sw.Elapsed.TotalMinutes < minutesToTrain && it < (int)(uint.MaxValue / tasks.Length))
                     {
@@ -67,14 +93,18 @@ namespace NeuralNetwork.Core
                         it++;
                         seed++;
                     }
+                    result.SeedsByThread[i] = it;
                 });
             }
             Task.WaitAll(tasks);
-
+            result.BestValue = bestValue;
+            result.TotalTime = sw.Elapsed.TotalMinutes;
             return best;
         }
         public BigDN? Train(BigDN network, double[][] inputs, double[][] targets)
         {
+            result = new TrainResult();
+            result.Threads = threads;
             BigDN? best = null;
             double bestValue = double.MaxValue;
             Task[] tasks = new Task[threads];
@@ -85,7 +115,7 @@ namespace NeuralNetwork.Core
             {
                 //Get the starting seed.
                 int seed = seedSteps[i];
-
+                result.SeedsByThread.Add(i, 0);
                 tasks[i] = Task.Run(() =>
                 {
                     int it = 0;//Iterations amount.
@@ -116,14 +146,18 @@ namespace NeuralNetwork.Core
                         it++;
                         seed++;
                     }
+                    result.SeedsByThread[i] = it;
                 });
             }
             Task.WaitAll(tasks);
-
+            result.BestValue = bestValue;
+            result.TotalTime = sw.Elapsed.TotalMinutes;
             return best;
         }
         public DeepNeural? Train(DeepNeural network, DataStream stream)
         {
+            result = new TrainResult();
+            result.Threads = threads;
             DeepNeural? best = null;
             double bestValue = double.MaxValue;
             Task[] tasks = new Task[threads];
@@ -134,6 +168,7 @@ namespace NeuralNetwork.Core
             {
                 //Get the starting seed.
                 int seed = seedSteps[i];
+                result.SeedsByThread.Add(i, 0);
                 tasks[i] = Task.Run(() =>
                 {
                     int it = 0;//Iterations amount.
@@ -164,14 +199,18 @@ namespace NeuralNetwork.Core
                         it++;
                         seed++;
                     }
+                    result.SeedsByThread[i] = it;
                 });
             }
             Task.WaitAll(tasks);
-
+            result.BestValue = bestValue;
+            result.TotalTime = sw.Elapsed.TotalMinutes;
             return best;
         }
         public DeepNeural? Train(DeepNeural network, double[][] inputs, double[][] targets)
         {
+            result = new TrainResult();
+            result.Threads = threads;
             DeepNeural? best = null;
             double bestValue = double.MaxValue;
             Task[] tasks = new Task[threads];
@@ -182,7 +221,7 @@ namespace NeuralNetwork.Core
             {
                 //Get the starting seed.
                 int seed = seedSteps[i];
-
+                result.SeedsByThread.Add(i, 0);
                 tasks[i] = Task.Run(() =>
                 {
                     int it = 0;//Iterations amount.
@@ -213,14 +252,18 @@ namespace NeuralNetwork.Core
                         it++;
                         seed++;
                     }
+                    result.SeedsByThread[i] = it;
                 });
             }
             Task.WaitAll(tasks);
-
+            result.BestValue = bestValue;
+            result.TotalTime = sw.Elapsed.TotalMinutes;
             return best;
         }
         public NeuralNetwork? Train(NeuralNetwork network, double[][] inputs, double[][] targets)
         {
+            result = new TrainResult();
+            result.Threads = threads;
             NeuralNetwork? best = null;
             double bestValue = double.MaxValue;
             Task[] tasks = new Task[threads];
@@ -239,7 +282,7 @@ namespace NeuralNetwork.Core
             {
                 //Get the starting seed.
                 int seed = seedSteps[i];
-
+                result.SeedsByThread.Add(i, 0);
                 tasks[i] = Task.Run(() =>
                 {
                     int it = 0;//Iterations amount.
@@ -270,10 +313,12 @@ namespace NeuralNetwork.Core
                         it++;
                         seed++;
                     }
+                    result.SeedsByThread[i] = it;
                 });
             }
             Task.WaitAll(tasks);
-
+            result.BestValue = bestValue;
+            result.TotalTime = sw.Elapsed.TotalMinutes;
             return best;
         }
     }
