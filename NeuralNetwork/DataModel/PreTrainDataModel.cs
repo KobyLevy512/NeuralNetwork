761260a807772c8e3c5ee6095191eb4993cfacf8
kobyl <kobyl@KOBY>
2024-11-15 12:13:14 +0200

    Add Pre Train data. Implement logic gates model.

---
+++ NeuralNetwork/DataModel/PreTrainDataModel.cs
@@ -0,0 +1,46 @@
+﻿
+namespace NeuralNetwork.DataModel
+{
+    public abstract class PreTrainDataModel
+    {
+        public abstract (double[][] inputs, double[][] outputs) GetData();
+
+        public class LogicGatesModel : PreTrainDataModel
+        {
+            public override (double[][] inputs, double[][] outputs) GetData()
+            {
+                var inputs = new double[12][]
+                {
+                    [0, 0, 0],
+                    [0, 0, 1],
+                    [0, 1, 0],
+                    [0, 1, 1],
+                    [1, 0, 0],
+                    [1, 0, 1],
+                    [1, 1, 0],
+                    [1, 1, 1],
+                    [2, 0, 0],
+                    [2, 0, 1],
+                    [2, 1, 0],
+                    [2, 1, 1],
+                };
+                var outputs = new double[12][]
+                {
+                    [0],
+                    [0],
+                    [0],
+                    [1],
+                    [0],
+                    [1],
+                    [1],
+                    [1],
+                    [0],
+                    [1],
+                    [1],
+                    [0],
+                };
+                return (inputs, outputs);
+            }
+        }
+    }
+}
